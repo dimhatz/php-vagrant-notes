675c75ed32a7e1cd72a5d477b8762b90030210b9
dimxdim <dimxdim@users.noreply.github.com>
2018-09-10 21:12:16 +0300

    config on xampp as default

--- public/debug.php
+++ public/debug.php
@@ -1,10 +1,12 @@
 <?php
-for ($i = 0; $i<5; $i++){
-    echo $i . "<br>";
 
-    // When output buffering is correctly disabled, this will result in line-by-line printing each second in browser.
-    // If it doesn't behave like this, it means that buffering is enabled (or compression or other settings in php
-    // or server are involved -> see my install-debug txt file for notes how to set it up)
-    sleep(1);
+
+$anumber = 1;
+
+function my(){
+  global $anumber;
+  ++$anumber;
 }
-phpinfo();
+
+my();
+echo "a is:" . $anumber . " " . $anumber;
